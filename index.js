8f3e833e44cfdd51b14770d483d38951984313f8
Pabmoy25 <pabmoy1725@gmail.com>
2024-03-11 19:30:47 -0300

    instalamos express y habilitamos los modulos

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
+import express from 'express';
+
 console.log (`hola mundo`)
 console.log (`hola mundo v2`)
+
+// 1 - Configurar un puerto
+
+// 2 - Configurar middlewares
+
+// 3 - Configurar las rutas
