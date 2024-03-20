e53af373ceaf2a3dcb75bfe4708081259bc3a477
Pabmoy25 <pabmoy1725@gmail.com>
2024-03-20 16:02:41 -0300

    agrego un ejemplo de ruta con una solicitud get

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 import express from 'express';
-
+import 'dotenv/config' //permite procesar variables de entorno
+import cors from 'cors'
+import morgan from 'morgan';
 
 // 1 - Configurar un puerto
 const app = express();
@@ -10,5 +12,17 @@ app.listen(app.get('port'), ()=>{
 })
 
 // 2 - Configurar middlewares
+app.use(cors()); //permite obtener conexiones remotas
+app.use (morgan('dev')); // nos da info extra en la terminal
+
+//todo: agregar el resto de middlewares
+
 
 // 3 - Configurar las rutas
+//http://localhost:4001/productos
+app.get('/', (req, res)=>{
+    //agregar toda la logica
+console.log('procesando una solicitud get');
+res.send('Respuesta del backend rollingcoffe')
+
+})
