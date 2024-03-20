bbd16515d6c0d78dd03ee79938d3bb6583d2018f
Pabmoy25 <pabmoy1725@gmail.com>
2024-03-20 16:32:08 -0300

    configuramos un archivo estatico en la ruta principal del backend

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-import express from 'express';
+import express, { urlencoded } from 'express';
 import 'dotenv/config' //permite procesar variables de entorno
 import cors from 'cors'
 import morgan from 'morgan';
+import path from 'path';
+import { fileURLToPath } from 'url'
 
 // 1 - Configurar un puerto
 const app = express();
@@ -14,6 +16,13 @@ app.listen(app.get('port'), ()=>{
 // 2 - Configurar middlewares
 app.use(cors()); //permite obtener conexiones remotas
 app.use (morgan('dev')); // nos da info extra en la terminal
+app.use (express.json()); // interpreta los datos en formato json
+app.use (express.urlencoded({extended:true})); // interpreta datos del body del request
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+/* console.log (__filename);
+console.log (path.join(__dirname, '/public')) */
+app.use (express.static(path.join(__dirname, '/public')))
 
 //todo: agregar el resto de middlewares
 
