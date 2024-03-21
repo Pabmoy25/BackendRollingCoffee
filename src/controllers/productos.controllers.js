0feaad8f71353978511920ef5dbac0c2222ed949
Pabmoy25 <pabmoy1725@gmail.com>
2024-03-21 09:47:24 -0300

    agregue la funcion obtener producto la cual usa un id por parametro

--- src/controllers/productos.controllers.js
+++ src/controllers/productos.controllers.js
@@ -1,40 +1,59 @@
 import Producto from "../database/models/producto.js";
 
+export const listarProductos = async (req, res) => {
+  try {
+    const productos = await Producto.find();
+    res.status(200).json(productos);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ mensaje: "Error al buscar los productos" });
+  }
+};
 
-export const listarProductos = async(req, res)=>{
-    try {
-       const productos = await Producto.find();
-       res.status(200).json(productos)
- 
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({mensaje:"Error al buscar los productos"})
-    }
-    
-
-}
-
-export const crearProductos = async(req, res)=>{
-    try {
-        // extraer los datos del body
-        /* console.log(req);
+export const crearProductos = async (req, res) => {
+  try {
+    // extraer los datos del body
+    /* console.log(req);
         console.log(req.body); */
 
-        //todo: validar los datos del body
-    
-    
-        // pedir a la BD crear el producto
-const productoNuevo = new Producto(req.body)
-await productoNuevo.save();
+    //todo: validar los datos del body
+
+    // pedir a la BD crear el producto
+    const productoNuevo = new Producto(req.body);
+    await productoNuevo.save();
 
     //responder al frontend
     res.status(201).json({
-        mensaje:'El producto fue creado exitosamente'
-    })
-    } catch (error) {
-        console.log(error)
-        res.status(400).json({
-            mensaje:'El producto no pudo ser creado'
-        })
+      mensaje: "El producto fue creado exitosamente",
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({
+      mensaje: "El producto no pudo ser creado",
+    });
+  }
+};
+
+
+export const obtenerProducto = async (req, res) => {
+  try {
+    //extraer el id
+    console.log(req.params.id);
+    // solicitar a la bd buscar ese producto
+    const productoBuscado = await Producto.findById(req.params.id)
+    // preguntar si no encontre el producto buscado
+    if (!productoBuscado) {
+      return res.status(404).json({ mensaje: "El producto con el id enviado no existe"})
     }
-    }
+
+    // enviar respuesta
+    res.status(200).json(productoBuscado);
+  } catch (error) {
+    console.log(error);
+    res
+      .status(400)
+      .json({ mensaje: "No se pudo encontrar el producto solicitado" })
+  }
+};
+
+
--- src/routes/productos.routes.js
+++ src/routes/productos.routes.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { crearProductos, listarProductos } from "../controllers/productos.controllers.js";
+import { crearProductos, listarProductos, obtenerProducto } from "../controllers/productos.controllers.js";
 
 const router = Router();
 
 // como creo las rutas
 router.route('/productos').get(listarProductos).post(crearProductos)
+router.route('/productos/:id').get(obtenerProducto)
 
 export default router;
